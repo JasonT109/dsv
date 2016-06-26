71d4704d7a665356464d0d43c5a6ec1033bde741
sehrstark <sehrstark@hotmail.com>
2016-06-27 08:48:35 +1200

    Live Camera test script
    Added live camera test script

---
+++ Assets/scripts/LiveCamera/testCameraHardcode.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testCameraHardcode : MonoBehaviour 
+{
+	public AVProLiveCamera _liveCamera;
+	public AVProLiveCameraManager _liveCameraManager;
+	//public GUISkin _guiSkin;
+	// Use this for initialization
+	void Start () 
+	{
+		
+		_liveCamera._desiredDeviceIndex = 8;
+		_liveCamera._desiredModeIndex = 20;
+		_liveCamera._deviceSelection = AVProLiveCamera.SelectDeviceBy.Index;
+		_liveCamera._modeSelection = AVProLiveCamera.SelectModeBy.Index;
+		_liveCamera.Begin();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
