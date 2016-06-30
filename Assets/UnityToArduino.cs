3b697bae2963d31d6f79a8be04280901ecd9da20
Mike Falkner <sehrstark@hotmail.com>
2016-07-01 08:13:49 +1200

    Arduino
    unity to arduino script

---
+++ Assets/UnityToArduino.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using System;
+
+public class UnityToArduino : MonoBehaviour 
+{
+    private SerialPort port;
+    public serverData Server;
+    public string COMPort = "COM6";
+
+    // initialization
+    void Start()
+    { 
+        port = new SerialPort(COMPort, 9600);
+        if (!port.IsOpen)
+        {
+            port.Open();
+            StartCoroutine(SendData());
+        }
+    }
+
+    IEnumerator SendData()
+    {
+        while (port.IsOpen)
+        {
+            port.Write(String.Format("{0}, {1}, {2}\0",
+                (int)(Server.yawAngle),
+                (int)(Server.pitchAngle),
+                (int)(Server.rollAngle))
+            ); 
+            yield return new WaitForSeconds(0.1f);
+        }
+    } 
+
+    void OnDestroy()
+    { 
+        port.Close(); 
+    }
+}
