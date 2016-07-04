a9727cf73a880de41fbe59bb8dea8aae47063096
Mike Falkner <sehrstark@hotmail.com>
2016-07-05 10:04:48 +1200

    fix noise rotation on sonar
    update sonar so that noise still appears to spin correctly on the sonar.

---
+++ Assets/NoiseRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoiseRotation : MonoBehaviour 
+{
+
+    public GameObject serverData;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 constrained = new Vector3(transform.localRotation.eulerAngles.x, serverData.transform.rotation.eulerAngles.y, transform.localRotation.eulerAngles.x);
+        transform.localRotation = Quaternion.Euler(constrained);
+	}
+}
--- Assets/scripts/EventSystem/megSonarEventManager.cs
+++ Assets/scripts/EventSystem/megSonarEventManager.cs
@@ -76,20 +76,50 @@
                 wps[i] = new Vector3(m.waypoints[i].x, m.waypoints[i].y + sonarYOffset, m.waypoints[i].z + sonarZOffset);
                 //wps[i] = new Vector3(m.waypoints[i].x, 10.0f, m.waypoints[i].z + sonarZOffset);
 
+                //rotate based on sonar anchor's angle
+                //Vector2 P1;
+                //P1.x = x;
+                //P1.y = z;
+                //
+                //Vector2 P2;
+                //P2.x = Anchor.transform.position.x + (x/4f);
+                //P2.y = Anchor.transform.position.z + (z/4f);
+                //
+                //Vector2 P3 = rotateAboutPoint2D(P1,P2, Anchor.transform.rotation.eulerAngles.y);
+
                 //float x = ((Anchor.transform.position.x + 40f)/80f) * 400f - 200f;
                 //float z = ((Anchor.transform.position.z + 40f)/80f) * 140f - 70f;
 
+                //normalise waypoint data
                 float x = (wps[i].x + 40)/80;
                 float z = (wps[i].z + -60)/30;
 
-                x = x * 400 - 200;
-                z = z * 140 - 70;
+                //convert to sonarscale
+                //x = x * 400 - 200;
+                //z = z * 140 - 70;
+
+                x = x * 110f - 55f;
+                z = z * 35f - 27.5f;
+
+                Vector2 PreRotate = new Vector2(dsvObject.transform.position.x + x, dsvObject.transform.position.z + z + 83f);
+
+                Vector2 PostRotate = rotateAboutPoint2D(PreRotate, new Vector2(dsvObject.transform.position.x, dsvObject.transform.position.z), -(dsvObject.transform.rotation.eulerAngles.y * 0.0174533f));
+
+                wps[i] = new Vector3(PostRotate.x, Anchor.transform.position.y + sonarYOffset -30.0f, PostRotate.y);
+
 
                 //convert waypoints from local space to world space
                 //wps[i] = dsvObject.transform.TransformPoint(wps[i]);
                 //wps[i] = new Vector3(dsvObject.transform.TransformPoint(wps[i]).x, Anchor.transform.position.y + sonarYOffset -30.0f, dsvObject.transform.TransformPoint(wps[i]).z);
                 //wps[i] = new Vector3(dsvObject.transform.TransformPoint(wps[i]).x, Anchor.transform.position.y + sonarYOffset -30.0f, dsvObject.transform.TransformPoint(wps[i]).z);
-                wps[i] = new Vector3(Anchor.transform.position.x + (x/4f), Anchor.transform.position.y + sonarYOffset -30.0f, Anchor.transform.position.z + (z/4f));
+
+                //wps[i] = new Vector3(Anchor.transform.localPosition.x + (x/4f), Anchor.transform.position.y + sonarYOffset -30.0f, Anchor.transform.localPosition.z + (z/4f));
+
+               // wps[i] = new Vector3(dsvObject.transform.TransformPoint(wps[i]).x, 
+               //     Anchor.transform.position.y + sonarYOffset -30.0f, 
+               //     dsvObject.transform.TransformPoint(wps[i]).z - 35f * (Mathf.Sin(dsvObject.transform.rotation.eulerAngles.x * 0.0174533f)));
+
+                //wps[i] = new Vector3(P3.x, Anchor.transform.position.y + sonarYOffset -30.0f, P3.y);
             }
 
             //spawn the main sonar object
@@ -135,6 +165,36 @@
         {
             yield return new WaitForSeconds(waitTime);
             canPress = true;
+        }
+
+        Vector2 rotateAboutPoint2D(Vector2 _p1, Vector2 _p2, float _angle)
+        {
+            //rotate about a point in 2D
+            //rotates _p1 about _p2
+            //Vector2 r;
+            //r.x = Mathf.Cos(_angle) * (_p2.x - _p1.x) - Mathf.Sin(_angle) * (_p2.y - _p1.y) + _p1.x;
+            //r.y = Mathf.Sin(_angle) * (_p2.x - _p1.x) + Mathf.Cos(_angle) * (_p2.y - _p1.y) + _p1.y;
+            //
+            ////
+            //return (r);
+
+
+
+            float s = Mathf.Sin(_angle);
+            float c = Mathf.Cos(_angle);
+
+            // translate point back to origin:
+            _p1.x -= _p2.x;
+            _p1.y -= _p2.y;
+
+            // rotate point
+            float xnew = _p1.x * c - _p1.y * s;
+            float ynew = _p1.x * s + _p1.y * c;
+
+            // translate point back:
+            _p1.x = xnew + _p2.x;
+            _p1.y = ynew + _p2.y;
+            return _p1;
         }
     }
 }
