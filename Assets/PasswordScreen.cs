49707c1a8f607cbe4166777ad79929009c4660cf
Mike Falkner <sehrstark@hotmail.com>
2016-07-05 11:25:37 +1200

    updated login screen for different subs
    tweaked login screen aesthetics
    tweaked login screen functionality
    added proper pin functionality
    fixed small bugs

--- Assets/PasswordScreen.cs
+++ Assets/PasswordScreen.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class PasswordScreen : MonoBehaviour 
 {
@@ -15,8 +16,14 @@ public class PasswordScreen : MonoBehaviour
     public Text HostIP;
     public Text HostIPTextPlaceholder;
 
+    public Image GliderButtonImg;
+    public Image BigSubButtonImg;
+    public Image DCCButtonImg;
+    int ID = 0; // ID of system, in this case it's set to Glider
+
     //public NetworkManagerCustom CustomNetMan;
     NetworkManagerCustom CustomNetMan;
+    public NetworkManager manager;
 
     public GameObject ZhangLogo;
     public GameObject StartButtonObj;
@@ -25,6 +32,13 @@ public class PasswordScreen : MonoBehaviour
     public string Password2 = "EarlGrey";
 
     public bool IsPasswordCorrect = false;
+    public bool IsPasswordPin = false;
+
+    public GameObject PasswordRoot;
+
+    public string GliderLevel = "screen_gliders";
+    public string BigSubLevel = "screen_01";
+    public string DCCLevel = "screen_gliders";
 
 
 	// Use this for initialization
@@ -32,22 +46,26 @@ public class PasswordScreen : MonoBehaviour
     {
         CustomNetMan = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<NetworkManagerCustom>();
 
-        ToggleClient();
+        ToggleHost();
         HostIP.text = Network.player.ipAddress;
         HostIPTextPlaceholder.text = Network.player.ipAddress;
 
         CustomNetMan.connectionInfo = Network.player.ipAddress;
         CustomNetMan.connectionList[0] = Network.player.ipAddress;
+     
+        ToggleBigSub();
 
         if(IsPasswordCorrect)
         {
             ZhangLogo.SetActive(true);
             StartButtonObj.SetActive(true);
+            PasswordRoot.SetActive(false);
         }
         else
         {
             ZhangLogo.SetActive(false);
             StartButtonObj.SetActive(false);
+            PasswordRoot.SetActive(true);
         }
 	}
 	
@@ -76,38 +94,82 @@ public class PasswordScreen : MonoBehaviour
         ClientButtonImg.color = Color.white;
     }
 
+    public void ToggleGlider()
+    {
+        ID = 0;
+        GliderButtonImg.color = Color.white;
+        BigSubButtonImg.color = Color.grey;
+        DCCButtonImg.color = Color.grey;
+        manager.onlineScene = GliderLevel;
+
+    }
+
+    public void ToggleBigSub()
+    {
+        ID = 2;
+        GliderButtonImg.color = Color.grey;;
+        BigSubButtonImg.color = Color.white;;
+        DCCButtonImg.color = Color.grey;
+        manager.onlineScene = BigSubLevel;
+    }
+
+    public void ToggleDCC()
+    {
+        ID = 3;
+        GliderButtonImg.color = Color.grey;;
+        BigSubButtonImg.color = Color.grey;;
+        DCCButtonImg.color = Color.white;
+        manager.onlineScene = DCCLevel;
+    }
+
+    public void PasswordLiveInput(string _PasswordInput)
+    {
+        if(IsPasswordPin)
+        {
+            PasswordInputChanged(_PasswordInput);
+        }
+    }
+
     public void PasswordInputChanged(string _PasswordInput)
     {
-        if(_PasswordInput == Password)
+        if(_PasswordInput != "")
         {
-            IsPasswordCorrect = true;
-            if(ZhangLogo)
+            if(_PasswordInput == Password)
             {
-                ZhangLogo.SetActive(true);
+                IsPasswordCorrect = true;
+                if(ZhangLogo)
+                {
+                    ZhangLogo.SetActive(true);
+                    PasswordRoot.SetActive(false);
+                }
+                StartButtonObj.SetActive(true);
             }
-            StartButtonObj.SetActive(true);
-        }
-        else if(_PasswordInput == Password2)
-        {
-            IsPasswordCorrect = true;
-            if(ZhangLogo)
+            else if(_PasswordInput == Password2)
             {
-                ZhangLogo.SetActive(true);
+                IsPasswordCorrect = true;
+                if(ZhangLogo)
+                {
+                    ZhangLogo.SetActive(true);
+                    PasswordRoot.SetActive(false);
+                }
+                StartButtonObj.SetActive(true);
             }
-            StartButtonObj.SetActive(true);
-        }
-        else
-        {
-            IsPasswordCorrect = false;
-            ZhangLogo.SetActive(false);
-            StartButtonObj.SetActive(false);
+            else
+            {
+                IsPasswordCorrect = false;
+                ZhangLogo.SetActive(false);
+                StartButtonObj.SetActive(false);
+            }
         }
     }
 
     public void HostIPChanged(string _HostIP)
     {
-        CustomNetMan.connectionInfo = _HostIP;
-        CustomNetMan.connectionList[0] = _HostIP;
+        if(_HostIP != "")
+        {
+            CustomNetMan.connectionInfo = _HostIP;
+            CustomNetMan.connectionList[0] = _HostIP;
+        }
     }
 
     public void StartButton()
