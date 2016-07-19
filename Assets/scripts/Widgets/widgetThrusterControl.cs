73d9cb87eb8fa6ac662609064739aa76a36a25e9
Steve Salmond <steve.salmond@gmail.com>
2016-07-20 09:42:53 +1200

    Integrating thruster gauges into revised Instruments screen.

--- Assets/scripts/Widgets/widgetThrusterControl.cs
+++ Assets/scripts/Widgets/widgetThrusterControl.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using Meg.Networking;
 
@@ -82,7 +82,7 @@ public class widgetThrusterControl : MonoBehaviour {
         thrusterMainR = Mathf.Clamp(inZ + (inX * 0.5f), -100, 100);
         thrusterSideL1 = Mathf.Clamp(-inY + (-inX * 0.5f) + inX2, -100, 100);
         thrusterSideR1 = Mathf.Clamp(-inY + (inX * 0.5f) + inX2, -100, 100);
-        thrusterSideL2 = Mathf.Clamp(-inX + inX2, -100, 100);
+        thrusterSideL2 = Mathf.Clamp(-inX - inX2, -100, 100);
         thrusterSideR2 = Mathf.Clamp(inX + inX2, -100, 100);
         thrusterSideL3 = Mathf.Clamp(inY + (-inX * 0.5f) + inX2, -100, 100);
         thrusterSideR3 = Mathf.Clamp(inY + (inX * 0.5f) + inX2, -100, 100);
@@ -90,111 +90,159 @@ public class widgetThrusterControl : MonoBehaviour {
         //assign the values to text objects
         thrusterVectorAngleL = inX * maxVectorAngle;
         thrusterVectorAngleR = inX * maxVectorAngle;
-        mainRPMTextL.text = (Mathf.Abs(thrusterMainL * 50)).ToString("n0") + " rpm";
-        mainRPMTextR.text = (Mathf.Abs(thrusterMainR * 50)).ToString("n0") + " rpm";
-        mainYawTextL.text = (thrusterVectorAngleL * 0.01f).ToString("n0") + "°";
-        mainYawTextR.text = (thrusterVectorAngleR * 0.01f).ToString("n0") + "°";
-        mainRPMTextSide1L.text = (Mathf.Abs(thrusterSideL1 * 50f)).ToString("n0") + " rpm";
-        mainRPMTextSide2L.text = (Mathf.Abs(thrusterSideL2 * 50f)).ToString("n0") + " rpm";
-        mainRPMTextSide3L.text = (Mathf.Abs(thrusterSideL3 * 50f)).ToString("n0") + " rpm";
-        mainRPMTextSide1R.text = (Mathf.Abs(thrusterSideR1 * 50f)).ToString("n0") + " rpm";
-        mainRPMTextSide2R.text = (Mathf.Abs(thrusterSideR2 * 50f)).ToString("n0") + " rpm";
-        mainRPMTextSide3R.text = (Mathf.Abs(thrusterSideR3 * 50f)).ToString("n0") + " rpm";
 
-        mainPowerTextSide1L.text = (Mathf.Abs(thrusterSideL1 * 1f)).ToString("n0") + " %";
-        mainPowerTextSide2L.text = (Mathf.Abs(thrusterSideL2 * 1f)).ToString("n0") + " %";
-        mainPowerTextSide3L.text = (Mathf.Abs(thrusterSideL3 * 1f)).ToString("n0") + " %";
-        mainPowerTextSide1R.text = (Mathf.Abs(thrusterSideR1 * 1f)).ToString("n0") + " %";
-        mainPowerTextSide2R.text = (Mathf.Abs(thrusterSideR2 * 1f)).ToString("n0") + " %";
-        mainPowerTextSide3R.text = (Mathf.Abs(thrusterSideR3 * 1f)).ToString("n0") + " %";
+        if (mainRPMTextL)
+            mainRPMTextL.text = (Mathf.Abs(thrusterMainL * 50)).ToString("n0") + " rpm";
+        if (mainRPMTextR)
+            mainRPMTextR.text = (Mathf.Abs(thrusterMainR * 50)).ToString("n0") + " rpm";
+        if (mainYawTextL)
+            mainYawTextL.text = (thrusterVectorAngleL * 0.01f).ToString("n0") + "°";
+        if (mainYawTextR)
+            mainYawTextR.text = (thrusterVectorAngleR * 0.01f).ToString("n0") + "°";
+        if (mainRPMTextSide1L)
+            mainRPMTextSide1L.text = (Mathf.Abs(thrusterSideL1 * 50f)).ToString("n0") + " rpm";
+        if (mainRPMTextSide2L)
+            mainRPMTextSide2L.text = (Mathf.Abs(thrusterSideL2 * 50f)).ToString("n0") + " rpm";
+        if (mainRPMTextSide3L)
+            mainRPMTextSide3L.text = (Mathf.Abs(thrusterSideL3 * 50f)).ToString("n0") + " rpm";
+        if (mainRPMTextSide1R)
+            mainRPMTextSide1R.text = (Mathf.Abs(thrusterSideR1 * 50f)).ToString("n0") + " rpm";
+        if (mainRPMTextSide2R)
+            mainRPMTextSide2R.text = (Mathf.Abs(thrusterSideR2 * 50f)).ToString("n0") + " rpm";
+        if (mainRPMTextSide3R)
+            mainRPMTextSide3R.text = (Mathf.Abs(thrusterSideR3 * 50f)).ToString("n0") + " rpm";
 
+        if (mainPowerTextSide1L)
+            mainPowerTextSide1L.text = (Mathf.Abs(thrusterSideL1 * 1f)).ToString("n0") + " %";
+        if (mainPowerTextSide2L)
+            mainPowerTextSide2L.text = (Mathf.Abs(thrusterSideL2 * 1f)).ToString("n0") + " %";
+        if (mainPowerTextSide3L)
+            mainPowerTextSide3L.text = (Mathf.Abs(thrusterSideL3 * 1f)).ToString("n0") + " %";
+        if (mainPowerTextSide1R)
+            mainPowerTextSide1R.text = (Mathf.Abs(thrusterSideR1 * 1f)).ToString("n0") + " %";
+        if (mainPowerTextSide2R)
+            mainPowerTextSide2R.text = (Mathf.Abs(thrusterSideR2 * 1f)).ToString("n0") + " %";
+        if (mainPowerTextSide3R)
+            mainPowerTextSide3R.text = (Mathf.Abs(thrusterSideR3 * 1f)).ToString("n0") + " %";
 
         if (thrusterMainL >= 0)
         {
-            tMainLPos.GetComponent<digital_gauge>().value = (int)thrusterMainL;
-            tMainLNeg.GetComponent<digital_gauge>().value = 0;
+            if (tMainLPos)
+                tMainLPos.GetComponent<digital_gauge>().value = (int)thrusterMainL;
+            if (tMainLNeg)
+                tMainLNeg.GetComponent<digital_gauge>().value = 0;
         }
         else
         {
-            tMainLPos.GetComponent<digital_gauge>().value = 0;
-            tMainLNeg.GetComponent<digital_gauge>().value = -(int)thrusterMainL;
+            if (tMainLPos)
+                tMainLPos.GetComponent<digital_gauge>().value = 0;
+            if (tMainLNeg)
+                tMainLNeg.GetComponent<digital_gauge>().value = -(int)thrusterMainL;
         }
 
         if (thrusterMainR >= 0)
         {
-            tMainRPos.GetComponent<digital_gauge>().value = (int)thrusterMainR;
-            tMainRNeg.GetComponent<digital_gauge>().value = 0;
+            if (tMainRPos)
+                tMainRPos.GetComponent<digital_gauge>().value = (int)thrusterMainR;
+            if (tMainRNeg)
+                tMainRNeg.GetComponent<digital_gauge>().value = 0;
         }
         else
         {
-            tMainRPos.GetComponent<digital_gauge>().value = 0;
-            tMainRNeg.GetComponent<digital_gauge>().value = -(int)thrusterMainR;
+            if (tMainRPos)
+                tMainRPos.GetComponent<digital_gauge>().value = 0;
+            if (tMainRNeg)
+                tMainRNeg.GetComponent<digital_gauge>().value = -(int)thrusterMainR;
         }
 
         if (thrusterSideL1 >= 0)
         {
-            tSideL1Pos.GetComponent<digital_gauge>().value = (int)thrusterSideL1;
-            tSideL1Neg.GetComponent<digital_gauge>().value = 0;
+            if (tSideL1Pos)
+                tSideL1Pos.GetComponent<digital_gauge>().value = (int)thrusterSideL1;
+            if (tSideL1Neg)
+                tSideL1Neg.GetComponent<digital_gauge>().value = 0;
         }
         else
         {
-            tSideL1Pos.GetComponent<digital_gauge>().value = 0;
-            tSideL1Neg.GetComponent<digital_gauge>().value = -(int)thrusterSideL1;
+            if (tSideL1Pos)
+                tSideL1Pos.GetComponent<digital_gauge>().value = 0;
+            if (tSideL1Neg)
+                tSideL1Neg.GetComponent<digital_gauge>().value = -(int)thrusterSideL1;
         }
 
         if (thrusterSideL2 >= 0)
         {
-            tSideL2Pos.GetComponent<digital_gauge>().value = (int)thrusterSideL2;
-            tSideL2Neg.GetComponent<digital_gauge>().value = 0;
+            if (tSideL2Pos)
+                tSideL2Pos.GetComponent<digital_gauge>().value = (int)thrusterSideL2;
+            if (tSideL2Neg)
+                tSideL2Neg.GetComponent<digital_gauge>().value = 0;
         }
         else
         {
-            tSideL2Pos.GetComponent<digital_gauge>().value = 0;
-            tSideL2Neg.GetComponent<digital_gauge>().value = -(int)thrusterSideL2;
+            if (tSideL2Pos)
+                tSideL2Pos.GetComponent<digital_gauge>().value = 0;
+            if (tSideL2Neg)
+                tSideL2Neg.GetComponent<digital_gauge>().value = -(int)thrusterSideL2;
         }
 
         if (thrusterSideL3 >= 0)
         {
-            tSideL3Pos.GetComponent<digital_gauge>().value = (int)thrusterSideL3;
-            tSideL3Neg.GetComponent<digital_gauge>().value = 0;
+            if (tSideL3Pos)
+                tSideL3Pos.GetComponent<digital_gauge>().value = (int)thrusterSideL3;
+            if (tSideL3Neg)
+                tSideL3Neg.GetComponent<digital_gauge>().value = 0;
         }
         else
         {
-            tSideL3Pos.GetComponent<digital_gauge>().value = 0;
-            tSideL3Neg.GetComponent<digital_gauge>().value = -(int)thrusterSideL3;
+            if (tSideL3Pos)
+                tSideL3Pos.GetComponent<digital_gauge>().value = 0;
+            if (tSideL3Neg)
+                tSideL3Neg.GetComponent<digital_gauge>().value = -(int)thrusterSideL3;
         }
 
         if (thrusterSideR1 >= 0)
         {
-            tSideR1Pos.GetComponent<digital_gauge>().value = (int)thrusterSideR1;
-            tSideR1Neg.GetComponent<digital_gauge>().value = 0;
+            if (tSideR1Pos)
+                tSideR1Pos.GetComponent<digital_gauge>().value = (int)thrusterSideR1;
+            if (tSideR1Neg)
+                tSideR1Neg.GetComponent<digital_gauge>().value = 0;
         }
         else
         {
-            tSideR1Pos.GetComponent<digital_gauge>().value = 0;
-            tSideR1Neg.GetComponent<digital_gauge>().value = -(int)thrusterSideR1;
+            if (tSideR1Pos)
+                tSideR1Pos.GetComponent<digital_gauge>().value = 0;
+            if (tSideR1Neg)
+                tSideR1Neg.GetComponent<digital_gauge>().value = -(int)thrusterSideR1;
         }
 
         if (thrusterSideR2 >= 0)
         {
-            tSideR2Pos.GetComponent<digital_gauge>().value = (int)thrusterSideR2;
-            tSideR2Neg.GetComponent<digital_gauge>().value = 0;
+            if (tSideR2Pos)
+                tSideR2Pos.GetComponent<digital_gauge>().value = (int)thrusterSideR2;
+            if (tSideR2Neg)
+                tSideR2Neg.GetComponent<digital_gauge>().value = 0;
         }
         else
         {
-            tSideR2Pos.GetComponent<digital_gauge>().value = 0;
-            tSideR2Neg.GetComponent<digital_gauge>().value = -(int)thrusterSideR2;
+            if (tSideR2Pos)
+                tSideR2Pos.GetComponent<digital_gauge>().value = 0;
+            if (tSideR2Neg)
+                tSideR2Neg.GetComponent<digital_gauge>().value = -(int)thrusterSideR2;
         }
 
         if (thrusterSideR3 >= 0)
         {
-            tSideR3Pos.GetComponent<digital_gauge>().value = (int)thrusterSideR3;
-            tSideR3Neg.GetComponent<digital_gauge>().value = 0;
+            if (tSideR3Pos)
+                tSideR3Pos.GetComponent<digital_gauge>().value = (int)thrusterSideR3;
+            if (tSideR3Neg)
+                tSideR3Neg.GetComponent<digital_gauge>().value = 0;
         }
         else
         {
-            tSideR3Pos.GetComponent<digital_gauge>().value = 0;
-            tSideR3Neg.GetComponent<digital_gauge>().value = -(int)thrusterSideR3;
+            if (tSideR3Pos)
+                tSideR3Pos.GetComponent<digital_gauge>().value = 0;
+            if (tSideR3Neg)
+                tSideR3Neg.GetComponent<digital_gauge>().value = -(int)thrusterSideR3;
         }
     }
 }
