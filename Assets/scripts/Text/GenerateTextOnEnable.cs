9839e894d4c5a25257baf292bff16cb6d8adbc2d
Steve Salmond <steve.salmond@gmail.com>
2016-07-22 10:35:36 +1200

    Only render terrain in the Navigation screen (to save FPS).

--- Assets/scripts/Text/GenerateTextOnEnable.cs
+++ Assets/scripts/Text/GenerateTextOnEnable.cs
@@ -3,18 +3,9 @@ using System.Collections;
 
 public class GenerateTextOnEnable : MonoBehaviour
 {
-    private bool _initial = true;
-
-    void OnDisable()
-    {
-        _initial = false;
-    }
 
     void OnEnable()
     {
-        // if (_initial)
-        //    return;
-
         // Rebuild all dynamic components as object becomes visible.
         var texts = GetComponentsInChildren<DynamicText>();
         foreach (var text in texts)
