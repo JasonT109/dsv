05a176a9f8c81a9e821420fb498b81c8ce29814b
Steve Salmond <steve.salmond@gmail.com>
2016-07-25 11:41:27 +1200

    Revising piloting HUD.

--- Assets/scripts/Widgets/artificialHorizon.cs
+++ Assets/scripts/Widgets/artificialHorizon.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using Meg.Networking;
 
@@ -11,6 +11,8 @@ public class artificialHorizon : MonoBehaviour {
     private Quaternion q;
     private float degreeDistance;
 
+    public bool reverse;
+
     void Start()
     {
         //localPosition distance for 1 degree
@@ -22,6 +24,7 @@ public class artificialHorizon : MonoBehaviour {
     {
         if (optionalHorizon)
         {
+
             if (isHorizonBall)
             {
                 Quaternion h = Quaternion.Euler(new Vector3(serverUtils.GetServerData("rollAngle"), 90, (serverUtils.GetServerData("pitchAngle") * 2f)));
@@ -32,7 +35,13 @@ public class artificialHorizon : MonoBehaviour {
                 optionalHorizon.transform.localPosition = new Vector3(0, (serverUtils.GetServerData("pitchAngle") * degreeDistance), optionalHorizon.transform.localPosition.z);
             }
         }
-        q = Quaternion.Euler(new Vector3(gameObject.transform.rotation.eulerAngles.x, gameObject.transform.rotation.eulerAngles.y, -serverUtils.GetServerData("rollAngle")));
+
+        var rollAngle = -serverUtils.GetServerData("rollAngle");
+        if (reverse)
+            rollAngle *= -1;
+
+        q = Quaternion.Euler(new Vector3(gameObject.transform.rotation.eulerAngles.x, gameObject.transform.rotation.eulerAngles.y, rollAngle));
+
         transform.rotation = q;
     }
 }
