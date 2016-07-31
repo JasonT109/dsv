046ae30e015795747be2ecc958ad9c74020f55eb
Steve Salmond <steve.salmond@gmail.com>
2016-08-01 07:51:43 +1200

    Reinstated map camera manager changes.

--- Assets/scripts/EventSystem/megMapCameraEventManager.cs
+++ Assets/scripts/EventSystem/megMapCameraEventManager.cs
@@ -38,149 +38,133 @@ public class megMapCameraEventManager : MonoBehaviour
 
     public void triggerByName(string eventName)
     {
-        for (int i = 0; i < mapEventList.Length; i++)
+        for (var i = 0; i < mapEventList.Length; i++)
         {
             if (mapEventList[i].eventName == eventName)
             {
-                serverUtils.SetServerData("initiateMapEvent", 0f);
+                StartRunningEvent(mapEventList[i]);
 
-                //start this event
-                running = true;
-
-                //set run time to 0
-                runTime = 0f;
-
-                //set the current running event
-                runningEvent = mapEventList[i];
-
-                //set the current running event button group just in case we want to turn this off when the event is complete
-                runningEventButton = mapEventList[i].trigger.GetComponent<buttonControl>();
-
-                //set the initial states
-                initialPosition = mapCameraRoot.transform.localPosition;
-                initialOrientationX = mapCameraPitch.transform.localRotation.eulerAngles.x;
-                initialOrientationY = mapCameraRoot.transform.localRotation.eulerAngles.y;
-                initialZoom = mapCameraObject.transform.localPosition.z;
-
-                //if to object get that position in map space
-                if (runningEvent.goToObject)
-                {
-                    var p = runningEvent.toObject.transform.position;
-                    runningEvent.toPosition = mapCameraRoot.transform.parent.InverseTransformPoint(p);
-                    runningEvent.toOrientation = new Vector3(initialOrientationX, initialOrientationY, 0);
-                    runningEvent.toZoom = initialZoom;
-                }
-
-                if (runningEvent.goToPlayerVessel)
-                {
-                    var playerVessel = serverUtils.GetPlayerVessel();
-                    var p = vessels[playerVessel - 1].transform.position;
-                    runningEvent.toPosition = mapCameraRoot.transform.parent.InverseTransformPoint(p);
-                    runningEvent.toOrientation = new Vector3(initialOrientationX, initialOrientationY, 0);
-                    runningEvent.toZoom = initialZoom;
-                }
+                // Toggle the event's associated button on if desired.
+                var b = runningEventButton;
+                if (b != null && b.toggleType && !b.active)
+                    b.RemoteToggle();
             }
         }
     }
 
-	void Update ()
+	private void Update()
     {
-        //check to see if the sever wants to start an event
-        if (serverUtils.GetServerData("initiateMapEvent") == 1.0f)
+        // Check to see if the server wants to start an event
+        if (Mathf.Approximately(serverUtils.GetServerData("initiateMapEvent"), 1))
         {
-            //trigger this event
-            triggerByName(serverUtils.GetServerDataAsText("mapEventName"));
+            var eventName = serverUtils.GetServerDataAsText("mapEventName");
+            if (runningEvent == null || runningEvent.eventName != eventName)
+                triggerByName(eventName);
         }
 
-        //we can trigger an event
-        for (int i = 0; i < mapEventList.Length; i++)
+        // Trigger camera events on button presses.
+        for (var i = 0; i < mapEventList.Length; i++)
         {
-            //check each button to see if it wants to trigger an event
             if (mapEventList[i].trigger.GetComponent<buttonControl>().pressed)
-            {
-                // Check if we are allowed to interrupt the current event.
-                if (running && mapEventList[i].priority < runningEvent.priority)
-                    continue;
-                
-                //start this event
-                running = true;
-
-                //set run time to 0
-                runTime = 0f;
-
-                //set the current running event
-                runningEvent = mapEventList[i];
-
-                //set the current running event button group just in case we want to turn this off when the event is complete
-                runningEventButton = mapEventList[i].trigger.GetComponent<buttonControl>();
-
-                //set the initial states
-                initialPosition = mapCameraRoot.transform.localPosition;
-                initialOrientationX = mapCameraPitch.transform.localRotation.eulerAngles.x;
-                initialOrientationY = mapCameraRoot.transform.localRotation.eulerAngles.y;
-                initialZoom = mapCameraObject.transform.localPosition.z;
-
-                //if to object get that position in map space
-                if (runningEvent.goToObject)
-                {
-                    var p = runningEvent.toObject.transform.position;
-                    runningEvent.toPosition = mapCameraRoot.transform.parent.InverseTransformPoint(p);
-                    runningEvent.toOrientation = new Vector3(initialOrientationX, initialOrientationY, 0);
-                    runningEvent.toZoom = initialZoom;
-                }
-
-                if (runningEvent.goToPlayerVessel)
-                {
-                    var playerVessel = serverUtils.GetPlayerVessel();
-                    var p = vessels[playerVessel - 1].transform.position;
-                    runningEvent.toPosition = mapCameraRoot.transform.parent.InverseTransformPoint(p);
-                    runningEvent.toOrientation = new Vector3(initialOrientationX, initialOrientationY, 0);
-                    runningEvent.toZoom = initialZoom;
-                }
-            }
+                StartRunningEvent(mapEventList[i]);
         }
 
-        if (running)
+        // Update the current running event (if any).
+        if (runningEvent != null)
         {
             runTime += Time.deltaTime;
             if (runTime > runningEvent.completeTime)
-            {
-                //stop this event
-                running = false;
-
-                // turn off the trigger button
-                if (!runningEventButton.buttonGroup)
-                    runningEventButton.active = false;
-            }
+                StopRunningEvent();
             else
-            {
-                //get percentage through the event
-                completePercentage = runTime / runningEvent.completeTime;
-
-                //set ease curve
-                completePercentage = graphicsEasing.EaseInOut(completePercentage, EasingType.Cubic);
-
-                //Lerp the camera root position
-                mapCameraRoot.transform.localPosition = Vector3.Lerp(initialPosition, runningEvent.toPosition, completePercentage);
-
-                //Lerp the camera y orientation
-                var currentY = Mathf.Repeat(initialOrientationY, 360);
-                var targetY = Mathf.Repeat(runningEvent.toOrientation.y, 360);
-                float camY = Mathf.LerpAngle(currentY, targetY, completePercentage);
-
-                //Lerp the camera pitch
-                float camX = Mathf.Lerp(initialOrientationX, runningEvent.toOrientation.x, completePercentage);
-
-                //set the root rotation
-                mapCameraRoot.transform.rotation = Quaternion.Euler(0, camY, 0);
-
-                //set the pitch
-                mapPitchSliderButton.transform.localPosition = new Vector3(graphicsMaths.remapValue(camX, sliderMinValue, sliderMaxValue, -1, 1) , 0, mapPitchSliderButton.transform.localPosition.z);
-
-                //Lerp the camera zoom
-                mapCameraObject.transform.localPosition = Vector3.Lerp(new Vector3(0, 0, initialZoom), new Vector3(0, 0, runningEvent.toZoom), completePercentage);
-
-            }
+                UpdateRunningEvent();
         }
 	}
+
+    /** Start running the given camera event. */
+    private void StartRunningEvent(megEventMapCamera e)
+    {
+        // Check if we are allowed to interrupt the current event.
+        if (runningEvent != null && e.priority < runningEvent.priority)
+            return;
+
+        // start this event
+        running = true;
+
+        // set run time to 0
+        runTime = 0f;
+
+        // set the current running event
+        runningEvent = e;
+
+        // set the current running event button group just in case we want to turn this off when the event is complete
+        runningEventButton = e.trigger.GetComponent<buttonControl>();
+
+        //set the initial states
+        initialPosition = mapCameraRoot.transform.localPosition;
+        initialOrientationX = mapCameraPitch.transform.localRotation.eulerAngles.x;
+        initialOrientationY = mapCameraRoot.transform.localRotation.eulerAngles.y;
+        initialZoom = mapCameraObject.transform.localPosition.z;
+
+        //if to object get that position in map space
+        if (runningEvent.goToObject)
+        {
+            var p = runningEvent.toObject.transform.position;
+            runningEvent.toPosition = mapCameraRoot.transform.parent.InverseTransformPoint(p);
+            runningEvent.toOrientation = new Vector3(initialOrientationX, initialOrientationY, 0);
+            runningEvent.toZoom = initialZoom;
+        }
+
+        if (runningEvent.goToPlayerVessel)
+        {
+            var playerVessel = serverUtils.GetPlayerVessel();
+            var p = vessels[playerVessel - 1].transform.position;
+            runningEvent.toPosition = mapCameraRoot.transform.parent.InverseTransformPoint(p);
+            runningEvent.toOrientation = new Vector3(initialOrientationX, initialOrientationY, 0);
+            runningEvent.toZoom = initialZoom;
+        }
+    }
+
+    /** Update the current camera event. */
+    private void UpdateRunningEvent()
+    {
+        // Get percentage through the event
+        completePercentage = runTime / runningEvent.completeTime;
+
+        // Set ease curve
+        completePercentage = graphicsEasing.EaseInOut(completePercentage, EasingType.Cubic);
+
+        // Lerp the camera root position
+        mapCameraRoot.transform.localPosition = Vector3.Lerp(initialPosition, runningEvent.toPosition, completePercentage);
+
+        // Lerp the camera y orientation
+        var currentY = Mathf.Repeat(initialOrientationY, 360);
+        var targetY = Mathf.Repeat(runningEvent.toOrientation.y, 360);
+        var camY = Mathf.LerpAngle(currentY, targetY, completePercentage);
+
+        // Lerp the camera pitch
+        var camX = Mathf.Lerp(initialOrientationX, runningEvent.toOrientation.x, completePercentage);
+
+        // Set the root rotation
+        mapCameraRoot.transform.rotation = Quaternion.Euler(0, camY, 0);
+
+        // Set the pitch
+        mapPitchSliderButton.transform.localPosition = new Vector3(graphicsMaths.remapValue(camX, sliderMinValue, sliderMaxValue, -1, 1), 0, mapPitchSliderButton.transform.localPosition.z);
+
+        // Lerp the camera zoom
+        mapCameraObject.transform.localPosition = Vector3.Lerp(new Vector3(0, 0, initialZoom), new Vector3(0, 0, runningEvent.toZoom), completePercentage);
+    }
+
+    /** Stop running the current camera event. */
+    private void StopRunningEvent()
+    {
+        //stop this event
+        running = false;
+        runningEvent = null;
+        runTime = 0f;
+
+        // turn off the trigger button
+        if (!runningEventButton.buttonGroup)
+            runningEventButton.active = false;
+    }
+
 }
