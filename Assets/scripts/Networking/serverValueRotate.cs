049f94c1cf18708ef84525e3c23ee1e66c860805
Steve Salmond <steve.salmond@gmail.com>
2016-08-12 15:50:50 +1200

    Working on Long range sonar screen.

--- Assets/scripts/Networking/serverValueRotate.cs
+++ Assets/scripts/Networking/serverValueRotate.cs
@@ -11,8 +11,16 @@ public class serverValueRotate : MonoBehaviour
     public float Offset = 0;
     public float Scale = 1;
 
+    private Quaternion _initialRotation;
+
     void OnEnable()
-        { UpdateTransform(); }
+    {
+        _initialRotation = transform.localRotation;
+        UpdateTransform();
+    }
+
+    void OnDisable()
+        { transform.localRotation = _initialRotation; }
 
     void LateUpdate()
         { UpdateTransform(); }
