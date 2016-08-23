1fa500b18cf51087d6e62a89c28bbf63b9b58c4c
sehrstark <sehrstark@hotmail.com>
2016-08-23 19:32:37 +1200

    Arduino added for SFX

--- Assets/scripts/Arduino/UnityToArduino.cs
+++ Assets/scripts/Arduino/UnityToArduino.cs
@@ -2,52 +2,69 @@ using UnityEngine;
 using System.Collections;
 using System.IO.Ports;
 using System;
-using Meg.Networking;
 
 public class UnityToArduino : MonoBehaviour 
 {
-    private SerialPort port;
-    public serverData Server;
-    public mapData Crew;
-    public string COMPort = "COM6";
+	private SerialPort port;
+	public serverData Server;
+	public SubControl Controls;
+	public string COMPort = "COM6";
 
-    // initialization
-    void Start()
-    { 
-        //Server = GameObject.FindGameObjectWithTag("ServerData").GetComponent<serverData>();
+	ArduinoManager Settings;
 
-        port = new SerialPort(COMPort, 9600);
-        if (!port.IsOpen)
-        {
-            port.Open();
-            StartCoroutine(SendData());
-        }
-            
-    }
+	// initialization
+	void Start()
+	{ 
+		if(GameObject.FindGameObjectWithTag("ArduinoManager"))
+		{
+			if(GameObject.FindGameObjectWithTag("ArduinoManager").GetComponent<ArduinoManager>())
+			{
+				Settings = GameObject.FindGameObjectWithTag("ArduinoManager").GetComponent<ArduinoManager>();
+				COMPort = Settings.ComPort;
+			
+				port = new SerialPort(COMPort, 115200);
+				if (!port.IsOpen)
+				{
+					port.Open();
+					StartCoroutine(SendData());
+				}
+			}
+		}
+			
 
-    IEnumerator SendData()
-    {
-        while (port.IsOpen)
-        {
-            //port.Write(String.Format("{0}, {1}, {2}\0",
-            //    (int)(Server.yawAngle),
-            //    (int)(Server.pitchAngle),
-            //    (int)(Server.rollAngle))); 
+	}
 
-            var playerVessel = serverUtils.GetPlayerVessel();
-            var position = serverUtils.GetVesselPosition(playerVessel);
+	void Awake()
+	{
+		//if(GameObject.FindGameObjectWithTag("ArduinoManager").GetComponent<ArduinoManager>())
+		//{
+		//	Settings = GameObject.FindGameObjectWithTag("ArduinoManager").GetComponent<ArduinoManager>();
+		//	COMPort = Settings.ComPort;
+		//}
+	}
+	IEnumerator SendData()
+	{
+		while (port.IsOpen)
+		{
 
-            port.Write(String.Format("{0}, {1}, {2}\0", 
-                (int)(position.x),
-                (int)(position.y),
-                (int)(position.z))); 
-            
-            yield return new WaitForSeconds(0.1f);
-        }
-    } 
 
-    void OnDestroy()
-    { 
-        port.Close(); 
-    }
+			port.Write(String.Format("${0},{1},{2},{3},{4},{5}\0",
+				(Server.yawAngle.ToString("F3")),
+				(Server.pitchAngle.ToString("F3")),
+				(Server.rollAngle.ToString("F3")),
+
+				(Controls.inputXaxis.ToString("F3")),
+				(Controls.inputYaxis.ToString("F3")),
+				(Controls.inputZaxis.ToString("F3")))
+			); 
+				
+
+			yield return new WaitForSeconds(0.0083f);
+		}
+	} 
+
+	void OnDestroy()
+	{ 
+		port.Close(); 
+	}
 }
