3b3eff4359f883cfb60cdc859a610dabd27f4d2b
JasonT109 <jason.tassell@gmail.com>
2016-08-27 18:20:24 +1200

    -DCC started Heller screens. Added some tabs.

---
+++ Assets/scripts/DCC/DCCMissionSwitch.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Meg.Networking;
+
+
+public class DCCMissionSwitch : MonoBehaviour
+{
+    public GameObject subsScreens;
+    public GameObject gliderScreens;
+
+    private int playerVessel = 1;
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (serverUtils.GetPlayerVessel() < 3)
+        {
+            subsScreens.SetActive(true);
+            gliderScreens.SetActive(false);
+        }
+        else
+        {
+            subsScreens.SetActive(false);
+            gliderScreens.SetActive(true);
+        }
+	}
+}
---
+++ Assets/scripts/Graphics/graphicsAnimateUV.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class graphicsAnimateUV : MonoBehaviour
+{
+
+    public float USpeed = 0f;
+    public float VSpeed = 0f;
+
+    private Renderer r;
+    public Vector2 offset = Vector2.one;
+
+    // Use this for initialization
+    void Start ()
+    {
+        r = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        offset.x += Time.deltaTime * USpeed;
+        offset.y += Time.deltaTime * VSpeed;
+
+        if (offset.x > 1)
+            offset.x = 0 + (1 - offset.x);
+        if (offset.x < 0)
+            offset.x = 1 - (offset.x);
+
+        if (offset.y > 1)
+            offset.y = 0 + (1 - offset.y);
+        if (offset.y < 0)
+            offset.y = 1 - (offset.y);
+
+        r.material.SetTextureOffset("_MainTex", offset);
+	}
+}
