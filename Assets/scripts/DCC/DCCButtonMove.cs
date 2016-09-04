62ee29df2a4e131d60c4f70139b67560429b20d4
noelgamo <noelgamo21@yahoo.com>
2016-09-05 09:43:49 +1200

    Button Move

---
+++ Assets/scripts/DCC/DCCButtonMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DCCButtonMove : MonoBehaviour
+{
+    public GameObject PlayerButton;
+    public GameObject TargetButton;
+	
+
+    public void Start()
+        {
+        PlayerButton.transform.position = TargetButton.transform.position;
+        }
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        PlayerButton.transform.position = Vector3.Lerp(PlayerButton.transform.position, TargetButton.transform.position, Time.time);
+	}
+}
