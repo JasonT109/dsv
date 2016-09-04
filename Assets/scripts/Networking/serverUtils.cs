5e83844682bb047596a23299f5993dbc88541f3f
Steve Salmond <steve.salmond@gmail.com>
2016-09-04 18:35:05 +1200

    Bumped version to 1.0.9 in preparation for next build.

--- Assets/scripts/Networking/serverUtils.cs
+++ Assets/scripts/Networking/serverUtils.cs
@@ -17,7 +17,7 @@ namespace Meg.Networking
         // ------------------------------------------------------------
 
         /** The current application version. */
-        public const string Version = "1.0.8";
+        public const string Version = "1.0.9";
 
         /** Return value representing an unknown server data value. */
         public const float Unknown = -1;
