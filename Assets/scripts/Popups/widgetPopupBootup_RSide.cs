78d1572cd1b39f1b288474ee196d72029322482b
noelgamo <noelgamo21@yahoo.com>
2016-09-06 09:55:24 +1200

    Returned the PNG sequence back to compressed and a little bit of timing change in the code

--- Assets/scripts/Popups/widgetPopupBootup_RSide.cs
+++ Assets/scripts/Popups/widgetPopupBootup_RSide.cs
@@ -81,7 +81,7 @@ public class widgetPopupBootup_RSide : widgetPopup
 
         Logo.gameObject.SetActive(true);
         Backdrop.gameObject.SetActive(true);
-        yield return new WaitForSeconds(7f);
+        yield return new WaitForSeconds(6.25f);
         Logo.gameObject.SetActive(false);
         Backdrop.gameObject.SetActive(false);
 
