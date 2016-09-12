315247f29967f0f2b108371708021ea28f9f4ec8
Steve Salmond <steve.salmond@gmail.com>
2016-09-12 15:02:08 +1200

    Implementing soft zoom limits a la Mike.

--- Assets/Plugins/TouchScript/Scripts/Behaviors/TransformerConstrained.cs
+++ Assets/Plugins/TouchScript/Scripts/Behaviors/TransformerConstrained.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using DG.Tweening;
 using TouchScript.Gestures;
 using UnityEngine;
 
@@ -18,7 +19,8 @@ namespace TouchScript.Behaviors
         public Bounds PanLimits;
 
         /** Zoom limits. */
-        public Vector2 ZoomLimits = new Vector2(0, float.MaxValue);
+        public Vector2 ZoomSoftLimits = new Vector2(0, float.MaxValue);
+        public float ZoomSmoothTime = 1;
 
 
         #endregion
@@ -26,7 +28,11 @@ namespace TouchScript.Behaviors
         #region Private variables
 
         private Transform cachedTransform;
-        private List<ITransformGesture> gestures = new List<ITransformGesture>();
+        private readonly List<ITransformGesture> gestures = new List<ITransformGesture>();
+        private Vector3 _lastOkPosition;
+        private Vector3 _lastOkScale;
+        private Vector3 _smoothPositionVelocity;
+        private Vector3 _smoothScaleVelocity;
 
         #endregion
 
@@ -47,6 +53,8 @@ namespace TouchScript.Behaviors
 
                 gestures.Add(transformGesture);
                 transformGesture.Transformed += transformHandler;
+                transformGesture.TransformStarted += transformStartedHandler;
+                transformGesture.TransformCompleted += transformCompletedHandler;
             }
         }
 
@@ -82,38 +90,63 @@ namespace TouchScript.Behaviors
             ApplyTransform(gesture, cachedTransform);
         }
 
+        private void transformStartedHandler(object sender, EventArgs e)
+        {
+        }
+
+        private void transformCompletedHandler(object sender, EventArgs e)
+        {
+        }
+
         /// <inheritdoc />
         private void ApplyTransform(TransformGesture gesture, Transform target)
         {
-            if (!Mathf.Approximately(gesture.DeltaScale, 1f))
+            target.localScale *= gesture.DeltaScale;
+
+            // Remember last acceptable values so we can reset to them.
+            var scale = target.localScale.x;
+            if (scale > ZoomSoftLimits.x && scale < ZoomSoftLimits.y)
             {
-                var scale = target.localScale * gesture.DeltaScale;
-                target.localScale = new Vector3(
-                    Mathf.Clamp(scale.x, ZoomLimits.x, ZoomLimits.y),
-                    Mathf.Clamp(scale.y, ZoomLimits.x, ZoomLimits.y),
-                    scale.z);
-
-                if (!Mathf.Approximately(scale.x, target.localScale.x))
-                    return;
-
-                if (!Mathf.Approximately(scale.y, target.localScale.y))
-                    return;
+                _lastOkScale = target.localScale;
+                _lastOkPosition = target.position;
+            }
+            else if (scale < ZoomSoftLimits.x)
+            {
+                target.localScale = new Vector3(ZoomSoftLimits.x, ZoomSoftLimits.x, target.localScale.z);
+                return;
             }
 
             if (!Mathf.Approximately(gesture.DeltaRotation, 0f))
-                target.rotation = Quaternion.AngleAxis(gesture.DeltaRotation, gesture.RotationAxis) * target.rotation;
+                target.rotation = Quaternion.AngleAxis(gesture.DeltaRotation, gesture.RotationAxis)*target.rotation;
 
             if (gesture.DeltaPosition != Vector3.zero)
             {
                 var s = target.localScale.x;
-                var min = target.parent.TransformPoint(PanLimits.min * s);
-                var max = target.parent.TransformPoint(PanLimits.max * s);
-
+                var min = target.parent.TransformPoint(PanLimits.min*s);
+                var max = target.parent.TransformPoint(PanLimits.max*s);
                 var p = target.position + gesture.DeltaPosition;
                 target.position = Vector3.Min(Vector3.Max(p, min), max);
             }
 
         }
+
+        private void LateUpdate()
+        {
+            // Return to acceptable values.
+            var scale = transform.localScale.x;
+            if (scale > ZoomSoftLimits.x && scale < ZoomSoftLimits.y)
+                return;
+
+            transform.localScale = Vector3.SmoothDamp(transform.localScale, 
+                _lastOkScale, ref _smoothScaleVelocity, ZoomSmoothTime);
+
+            transform.position = Vector3.SmoothDamp(transform.position, 
+                _lastOkPosition, ref _smoothPositionVelocity, ZoomSmoothTime);
+        }
+
+        private static float RemapValue(float value, float inMin, float inMax, float outMin, float outMax)
+            { return ((value - inMin) / (inMax - inMin) * (outMax - outMin)) + outMin; }
+
 
         #endregion
     }
