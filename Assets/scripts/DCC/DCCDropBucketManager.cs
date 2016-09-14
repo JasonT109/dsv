de1ad7ad889dc418298e1ac7bfd4fa635fe3309d
JasonT109 <jason.tassell@gmail.com>
2016-09-15 11:16:12 +1200

    -dcc top task bar prefabbed with drop buckets. -added drop bucket manager that sets correct number of buckets for the station.

---
+++ Assets/scripts/DCC/DCCDropBucketManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class DCCDropBucketManager : MonoBehaviour
+{
+    public GameObject dropBucketGroup1;
+    public GameObject dropBucketGroup2;
+
+    private static bool HasScreen(screenData.Type type)
+    {
+        return DCCScreenData.GetStationHasScreen(DCCScreenData.StationId, type);
+    }
+
+    void Update ()
+    {
+        if (HasScreen(screenData.Type.DccScreen4))
+        {
+            dropBucketGroup1.SetActive(true);
+            dropBucketGroup2.SetActive(false);
+        }
+        else
+        {
+            dropBucketGroup1.SetActive(false);
+            dropBucketGroup2.SetActive(true);
+        }
+	}
+}
