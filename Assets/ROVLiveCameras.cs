fdeb794888a9d17c80a3d0681775f79c709facb6
Mike Falkner <sehrstark@hotmail.com>
2016-09-26 02:28:02 +1300

    live feed finished ROV

--- Assets/ROVLiveCameras.cs
+++ Assets/ROVLiveCameras.cs
@@ -1,16 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Meg.Networking;
 
 public class ROVLiveCameras : MonoBehaviour
 {
+    public AVProLiveCameraUGUIComponent[] AVCameraOutputs;
+    public LiveCameraOutputFinder Outputs;
 
-	// Use this for initialization
-	void Start () {
-	
+    public int iCurrentMain = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+	    
 	}
+
+    void OnEnable()
+    {
+        if(Outputs.isLive)
+        {
+            AVCameraOutputs[0].m_liveCamera = Outputs.GetOutput(iCurrentMain);
+            AVCameraOutputs[1].m_liveCamera = Outputs.GetOutput(0);
+            AVCameraOutputs[2].m_liveCamera = Outputs.GetOutput(1);
+            AVCameraOutputs[3].m_liveCamera = Outputs.GetOutput(2);
+            AVCameraOutputs[4].m_liveCamera = Outputs.GetOutput(3);
+        }
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        if(!serverUtils.IsReady())
+        {
+            return;
+        }
+
+        if (serverUtils.SubControl.isControlDecentMode && serverUtils.OSRov.ROVCameraState == 1)
+        {
+            serverUtils.SubControl.isControlDecentMode = false;
+            iCurrentMain++;
+            if(iCurrentMain > 3)
+            {
+                iCurrentMain = 0;
+            }
+            AVCameraOutputs[0].m_liveCamera = Outputs.GetOutput(iCurrentMain);
+
+        }
 	}
 }
