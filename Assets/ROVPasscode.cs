e1acb3a93b406791b777004a600ddc1cc41f3bc6
Mike Falkner <sehrstark@hotmail.com>
2016-09-25 16:32:30 +1300

    ROV Update

--- Assets/ROVPasscode.cs
+++ Assets/ROVPasscode.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ROVPasscode : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class ROVPasscode : MonoBehaviour
     public GameObject Key2;
     public GameObject Key3;
     public GameObject Key4;
+
+    public GameObject LiveButton;
 
     int iKeysEntered = 0;
 
@@ -20,7 +23,13 @@ public class ROVPasscode : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-	    
+	    if(iKeysEntered == 0)
+        {
+            Key1.SetActive(false);
+            Key2.SetActive(false);
+            Key3.SetActive(false);
+            Key4.SetActive(false);
+        }
 	}
 
     public void Reset()
@@ -37,9 +46,62 @@ public class ROVPasscode : MonoBehaviour
 
         }
 
-        if(iKeysEntered >4)
+        if(iKeysEntered > 4)
         {
             iKeysEntered = 4;
+        }
+
+        if(iKeysEntered == 1)
+        {
+            Key1.GetComponent<Image>().color = new Color(1f, 225f / 255f, 0f);
+            Key2.GetComponent<Image>().color = new Color(1f, 225f / 255f, 0f);
+            Key3.GetComponent<Image>().color = new Color(1f, 225f / 255f, 0f);
+            Key4.GetComponent<Image>().color = new Color(1f, 225f / 255f, 0f);
+
+            Key1.SetActive(true);
+            Key2.SetActive(false);
+            Key3.SetActive(false);
+            Key4.SetActive(false);
+
+        }
+
+        if (iKeysEntered == 2)
+        {
+            Key1.GetComponent<Image>().color = new Color(1f, 225f / 255f, 0f);
+            Key2.GetComponent<Image>().color = new Color(1f, 225f / 255f, 0f);
+            Key3.GetComponent<Image>().color = new Color(1f, 225f / 255f, 0f);
+            Key4.GetComponent<Image>().color = new Color(1f, 225f / 255f, 0f);
+
+            Key1.SetActive(true);
+            Key2.SetActive(true);
+            Key3.SetActive(false);
+            Key4.SetActive(false);
+        }
+
+        if (iKeysEntered == 3)
+        {
+            Key1.GetComponent<Image>().color = new Color(1f, 225f / 255f, 0f);
+            Key2.GetComponent<Image>().color = new Color(1f, 225f / 255f, 0f);
+            Key3.GetComponent<Image>().color = new Color(1f, 225f / 255f, 0f);
+            Key4.GetComponent<Image>().color = new Color(1f, 225f / 255f, 0f);
+
+            Key1.SetActive(true);
+            Key2.SetActive(true);
+            Key3.SetActive(true);
+            Key4.SetActive(false);
+        }
+
+        if (iKeysEntered == 4)
+        {
+            Key1.GetComponent<Image>().color = new Color(0.9f, 0f, 0f);
+            Key2.GetComponent<Image>().color = new Color(0.9f, 0f, 0f);
+            Key3.GetComponent<Image>().color = new Color(0.9f, 0f, 0f);
+            Key4.GetComponent<Image>().color = new Color(0.9f, 0f, 0f);
+
+            Key1.SetActive(true);
+            Key2.SetActive(true);
+            Key3.SetActive(true);
+            Key4.SetActive(true);
         }
     }
 
