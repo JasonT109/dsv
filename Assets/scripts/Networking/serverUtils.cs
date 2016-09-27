a14e5b97dad8feae140dd54915f1b669a92246fc
Steve Salmond <steve.salmond@outsmartgames.com>
2016-09-27 15:25:01 +1300

    Minor adjustments to strategy table.

--- Assets/scripts/Networking/serverUtils.cs
+++ Assets/scripts/Networking/serverUtils.cs
@@ -17,7 +17,7 @@ namespace Meg.Networking
         // ------------------------------------------------------------
 
         /** The current application version. */
-        public const string Version = "1.3.3";
+        public const string Version = "1.3.4";
 
         /** Return value representing an unknown server data value. */
         public const float Unknown = -1;
