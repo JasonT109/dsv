ccb7c3b2e6be599b70e69599a6a4ffc0c7634c5e
JasonT109 <jason.tassell@gmail.com>
2016-10-05 18:30:10 +1300

    Added a blur to the quad screen when a window is full screen.

---
+++ Assets/scripts/DCC/DCCQuadFullBlur.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DCCQuadFullBlur : MonoBehaviour
+{
+    public GameObject blurObject;
+
+    public DCCScreenManager manager;
+
+	void Start ()
+    {
+        if (!manager)
+            manager = ObjectFinder.Find<DCCScreenManager>();
+	}
+
+	void Update ()
+    {
+        if (manager && blurObject)
+        {
+            for (int i = 0; i < manager.quadBoxes.Length; i++)
+            {
+                if (manager.quadBoxes[i].boxPosition == Meg.DCC.DCCScreenContentPositions.positionID.middle)
+                {
+                    if (manager.quadBoxes[i].boxContent != DCCWindow.contentID.none)
+                        blurObject.SetActive(true);
+                    else
+                        blurObject.SetActive(false);
+                }
+            }
+        }
+	}
+}
