7b7d965dcc99e064dfed78c0a27ae84739330a9b
Steve Salmond <steve.salmond@outsmartgames.com>
2016-10-07 18:31:05 +1300

    Corrected station overhead screen setup in the configuration file.

--- Assets/scripts/Networking/serverUtils.cs
+++ Assets/scripts/Networking/serverUtils.cs
@@ -17,7 +17,7 @@ namespace Meg.Networking
         // ------------------------------------------------------------
 
         /** The current application version. */
-        public const string Version = "1.4.5";
+        public const string Version = "1.4.6";
 
         /** Return value representing an unknown server data value. */
         public const float Unknown = -1;
