2d07d67b5ce9c28db8abb63ca1d355670ca1913b
JasonT109 <jason.tassell@gmail.com>
2016-10-19 09:32:28 +1300

    Versioning to 1.5.1.

--- Assets/scripts/Networking/serverUtils.cs
+++ Assets/scripts/Networking/serverUtils.cs
@@ -17,7 +17,7 @@ namespace Meg.Networking
         // ------------------------------------------------------------
 
         /** The current application version. */
-        public const string Version = "1.5.0";
+        public const string Version = "1.5.1";
 
         /** Return value representing an unknown server data value. */
         public const float Unknown = -1;
